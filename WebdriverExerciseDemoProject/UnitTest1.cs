cdc62705776003da255a5ea6e4454624ced8ef3b
Martin <skylinecho@abv.bg>
2025-02-05 11:12:56 +0200

    Adding webdriver wait demo

---
+++ WebdriverExerciseDemoProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WebdriverExerciseDemoProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
