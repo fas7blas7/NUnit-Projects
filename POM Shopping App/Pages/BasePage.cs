fc2880eae1763c3243a58c7ad0af8cf89bc59039
Martin <skylinecho@abv.bg>
2025-02-06 21:41:22 +0200

    Adding Shopping app project with POM approach

---
+++ POM Shopping App/Pages/BasePage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Collections.ObjectModel;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace POM_Exercise.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+
+        protected WebDriverWait wait;
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        protected IWebElement FindElement(By by)
+        {
+            return wait.Until(ExpectedConditions.ElementIsVisible(by));
+        }
+
+        protected ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            return driver.FindElements(by);
+        }
+
+        protected void Click(By by)
+        {
+            FindElement(by).Click();
+        }
+
+        protected void Type(By by, string text) 
+        {
+            var element = FindElement(by);
+            element.Clear();
+            element.SendKeys(text);
+        }
+
+        protected string GetText(By by) 
+        {
+            var element = FindElement(by);
+            return element.Text;
+        }
+    }
+}
---
+++ POM Shopping App/Pages/LoginPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POM_Exercise.Pages
+{
+    public class LoginPage : BasePage
+    {
+        private readonly By userNameField = By.Id("user-name");
+        private readonly By passwordField = By.Id("password");
+        private readonly By loginButton = By.Id("login-button");
+        private readonly By errorMessage = By.XPath("//div[@class='error-message-container error']//h3");
+    }
+
+    public LoginPage(IWebDriver driver) : base(driver)
+    {
+
+    }
+
+    public void FillUserName(string username)
+    {
+        Type(userNameField, username);
+    }
+}
